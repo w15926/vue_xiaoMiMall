9604a446cc8e06811e7370886d6524b19ebd465f
w15926 <1565424597@qq.com>
2021-01-31 14:57:14 +0800

    项目目录结构划分

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,11 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
+const Home = () => import('@/views/Home.vue')
+
 const routes = [
+  { path: '', redirect: '/home' },
+  { path: '/home', component: Home }
 ]
 
 const router = new VueRouter({
